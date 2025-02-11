9464b97cba135382f6a582a79d8db717cbf1475e
sdmlr <sdmlr_@hotmail.com>
2025-02-11 14:49:46 +0000

    Add router

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,8 +1,15 @@
 <script setup>
 import JobListing from "@/components/JobListing.vue";
 import jobData from "@/jobs.json";
-import { ref } from "vue";
+import { ref, defineProps } from "vue";
 
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false
+  }
+});
 const jobs = ref(jobData);
 </script>
 
@@ -13,8 +20,19 @@ const jobs = ref(jobData);
         Browse Jobs
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+        <JobListing
+          v-for="job in jobs.slice(0, limit || jobs.length)"
+          :key="job.id"
+          :job="job"
+        />
       </div>
     </div>
   </section>
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+      <a
+        href="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
 </template>
