ae7df7ab2af90d107c8b909db093446271e36845
sdmlr <sdmlr_@hotmail.com>
2025-02-11 15:48:34 +0000

    Add Not-Found page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import HomeView from '@/views/HomeView.vue';
 import JobsView from '@/views/JobsView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
             name: 'jobs',
             component: JobsView,
         },
+        {
+            path: '/:catchAll(.*)',
+            name: 'not-found',
+            component: NotFoundView,
+        }
     ],
 });
 
