0c830064e9f0fcccad497ce60a1d44d919f195a0
sdmlr <sdmlr_@hotmail.com>
2025-02-11 19:07:23 +0000

    toast setup

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import './assets/main.css'
-import 'primeicons/primeicons.css'
-import router from './router'
+import "./assets/main.css";
+import "primeicons/primeicons.css";
+import Toast from "vue-toastification";
+import 'vue-toastification/dist/index.css'
+import router from "./router";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(router);
+app.use(Toast);
 
-app.mount('#app')
+app.mount("#app");
